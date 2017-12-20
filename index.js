3eb3a5ed316bde7d3c44ba1b2c56aeb5a84716e9
John York <jmyork762@gmail.com>
2017-12-20 19:13:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.") return "I love you, too";
-  if (string === lowercase)
-  if (string === uppercase) 
+  if (string === lowercase) return "I can't hear you!"
+  if (string === uppercase) return
 }
